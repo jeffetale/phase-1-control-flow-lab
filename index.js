6f54214b280f6e5522f6ceae96f1e27e4ef5da41
jeff <jeff.etale@student.moringaschool.com>
2023-06-13 15:38:09 +0300

    Done assignment

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride){
   // Write your code here!
+  let price;
+ if (ride <= 400) {
+  price = 'This one is on me!';
+ } else if (ride > 2000 && ride <= 2500) {
+  price = 'I will gladly take your thirty bucks.'
+ } else if (ride > 2500) {
+  price = 'No can do.';
+ }
+ return price;
+
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city) {
   // Write your code here!
+  let reply;
+  if (city === "NYC" ) {
+    reply = "Ok, sounds good.";
+  } else  {
+    reply = "No go.";
+  }
+    return reply;
+  
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip) {
   // Write your code here!
+let byeMessage;
+if (tip === "generous") {
+  byeMessage = "Thank you so much."
+} else if (tip === "not as generous") {
+  byeMessage = "Thank you."
+} else {
+  byeMessage = "Bye."
+}
+  return byeMessage;
 }
